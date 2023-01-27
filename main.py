9373f5d02924dc75f5e6be709bd53438a0231d4a
smergeolacx <anish.solleti@gmail.com>
2023-01-27 16:53:30 +0530

    line-line intersection

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 height, width = 800, 800
 screen = pygame.display.set_mode((width, height))
 
-raycast = Ray(200, 400, screen)
+ray_cast = Ray(200, 400, screen)
 obstacle = Wall(600, 200, 600, 600, screen)
 
 while True:
@@ -19,8 +19,23 @@ while True:
             exit()
 
     screen.fill("black")
-    raycast.show()
-    # raycast.point(pygame.mouse.get_pos())
+    ray_cast.show()
+    ray_cast.point(pygame.mouse.get_pos())
     obstacle.show()
+
+    x1, y1 = ray_cast.pos.x, ray_cast.pos.y
+    x2, y2 = pygame.mouse.get_pos()
+    x3, y3 = obstacle.a.x, obstacle.a.y
+    x4, y4 = obstacle.b.x, obstacle.b.y
+    denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+    t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
+    u = ((x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2)) / denom
+
+    if denom != 0:
+        if 0 <= t <= 1 and 0 <= u <= 1:
+            print(True)
+    else:
+        pass
+
     clock.tick(60)
     pygame.display.flip()
